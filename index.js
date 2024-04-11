34c4efca2d4a6bf17f485b9090621d981b099a02
Lucas Alves dos Santos <lucas_ads@hotmail.com.br>
2024-04-10 22:06:54 -0400

    criação do model Usuario e execução do primeiro sync

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
 const conn = require("./db/conn");
 
+const Usuario = require("./models/Usuario");
+
 conn
-  .authenticate()
+  .sync()
   .then(() => {
-    console.log("Conectado ao banco de dados com sucesso!");
+    console.log("Conectado e sincronizado com o banco de dados!");
   })
   .catch((err) => {
     console.log("Ocorreu um erro: " + err);
   });
+
+// conn
+//   .authenticate()
+//   .then(() => {
+//     console.log("Conectado ao banco de dados com sucesso!");
+//   })
+//   .catch((err) => {
+//     console.log("Ocorreu um erro: " + err);
+//   });
---
+++ models/Usuario.js
@@ -0,0 +1,15 @@
+const db = require("../db/conn");
+const { DataTypes } = require("sequelize");
+
+const Usuario = db.define("Usuario", {
+  nickname: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+});
+
+module.exports = Usuario;
