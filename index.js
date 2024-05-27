b98a6af43a4aac198e9a6012fcfa46b07d875f99
Lucas Alves dos Santos <lucas_ads@hotmail.com.br>
2024-05-27 13:27:48 -0400

    Adição do handlebars - Cadastro e listagem

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
+// Importações de módulos:
 require("dotenv").config();
 const conn = require("./db/conn");
-
 const Usuario = require("./models/Usuario");
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+// Instanciação do servidor:
+const app = express();
+
+// Vinculação do Handlebars ao Express:
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// Configurações no express para facilitar a captura
+// de dados recebidos de formulários
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/usuarios", async (req, res) => {
+  const usuarios = await Usuario.findAll({ raw: true });
+
+  res.render("usuarios", { usuarios });
+});
+
+app.get("/usuarios/novo", (req, res) => {
+  res.render("formUsuario");
+});
+
+app.post("/usuarios/novo", async (req, res) => {
+  const dadosUsuario = {
+    nickname: req.body.nickname,
+    nome: req.body.nome,
+  };
+
+  const usuario = await Usuario.create(dadosUsuario);
+  res.send("Usuário inserido sob o id " + usuario.id);
+});
+
+app.listen(8000, () => {
+  console.log("Server rodando!");
+});
 
 conn
   .sync()
